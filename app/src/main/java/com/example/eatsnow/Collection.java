2921d13b1920caaa44f51615e84a78c5477c93cc
iam844 <jaiswaladitya844@gmail.com>
2019-05-09 16:14:00 +0530

    Spelling check

--- app/src/main/java/com/example/eatsnow/Collection.java
+++ app/src/main/java/com/example/eatsnow/Collection.java
@@ -4,43 +4,44 @@ public class Collection {
     private int collection_id;
     private String title;
     private String url;
-    private String discription;
+    private String description;
     private String image_url;
     private int res_count;
     private String share_url;
 
-    public Collection(int collection_id,String title,String url, String discription, String image_url,int res_count, String share_url){
+    //Setter functions
+    public Collection(int collection_id, String title, String url, String description, String image_url, int res_count, String share_url) {
         this.collection_id = collection_id;
         this.title = title;
         this.url = url;
-        this.discription = discription;
+        this.description = description;
         this.image_url = image_url;
         this.res_count = res_count;
         this.share_url = share_url;
     }
 
     //Getter functions
-    public int getCollection_id(){
+    public int getCollection_id() {
         return collection_id;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public String getUrl(){
+    public String getUrl() {
         return url;
     }
 
-    public String getDiscription(){
-        return discription;
+    public String getDescription() {
+        return description;
     }
 
-    public String getImage_url(){
+    public String getImage_url() {
         return image_url;
     }
 
-    public int getRes_count(){
+    public int getRes_count() {
         return res_count;
     }
 
