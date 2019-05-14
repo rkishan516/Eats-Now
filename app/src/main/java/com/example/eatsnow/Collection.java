37ed71580008115de22422757761fa490b306361
rkishan516 <34465683+rkishan516@users.noreply.github.com>
2019-05-14 10:59:58 +0530

    Corrected Speeling
    Corrected the spelling of description

--- app/src/main/java/com/example/eatsnow/Collection.java
+++ app/src/main/java/com/example/eatsnow/Collection.java
@@ -10,7 +10,7 @@ public class Collection {
     private String share_url;
   
     //Constructor Function
-    public Collection(int collection_id,String title,String url, String discription, String image_url,int res_count, String share_url){
+    public Collection(int collection_id,String title,String url, String description, String image_url,int res_count, String share_url){
         this.collection_id = collection_id;
         this.title = title;
         this.url = url;
