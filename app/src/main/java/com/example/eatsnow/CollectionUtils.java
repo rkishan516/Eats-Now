5699e83322cbd1f96cecb7033e84d37ff4523094
kishan <rkishan516@gmail.com>
2019-05-14 10:45:02 +0530

    Added DailyMenu and Related Classes

--- app/src/main/java/com/example/eatsnow/CollectionUtils.java
+++ app/src/main/java/com/example/eatsnow/CollectionUtils.java
@@ -7,33 +7,30 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Helper methods related to requesting and receiving Data from Zomato API.
  */
-public final class QueryUtils {
+public final class CollectionUtils extends Network {
 
-    /** Tag for the log messages */
-    private static final String LOG_TAG = QueryUtils.class.getSimpleName();
 
     /**
-     * Create a private constructor because no one should ever create a {@link QueryUtils} object.
+     * Create a private constructor because no one should ever create a {@link CollectionUtils} object.
      * This class is only meant to hold static variables and methods, which can be accessed
-     * directly from the class name QueryUtils (and an object instance of QueryUtils is not needed).
+     * directly from the class name QueryUtils (and an object instance of CollectionUtils is not needed).
      */
-    private QueryUtils() {
+    private CollectionUtils() {
     }
 
+    /**
+     * Create a fetchCollectionData because fetch the top collection of Restaurents
+     * @param requestUrl
+     * @return List of {@link Collection}
+     */
     public static List<Collection> fetchCollectionData(String requestUrl) {
         // Create URL object
         URL url = createUrl(requestUrl);
@@ -53,80 +50,7 @@ public final class QueryUtils {
         return collections;
     }
 
-    /**
-     * Returns new URL object from the given string URL.
-     */
-    private static URL createUrl(String stringUrl) {
-        URL url = null;
-        try {
-            url = new URL(stringUrl);
-        } catch (MalformedURLException e) {
-            Log.e(LOG_TAG, "Problem building the URL ", e);
-        }
-        return url;
-    }
 
-    /**
-     * Make an HTTP request to the given URL and return a String as the response.
-     */
-    private static String makeHttpRequest(URL url) throws IOException {
-        String jsonResponse = "";
-
-        // If the URL is null, then return early.
-        if (url == null) {
-            return jsonResponse;
-        }
-
-        HttpURLConnection urlConnection = null;
-        InputStream inputStream = null;
-        try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setReadTimeout(10000 /* milliseconds */);
-            urlConnection.setConnectTimeout(15000 /* milliseconds */);
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            // If the request was successful (response code 200),
-            // then read the input stream and parse the response.
-            if (urlConnection.getResponseCode() == 200) {
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
-            } else {
-                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
-            }
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (inputStream != null) {
-                // Closing the input stream could throw an IOException, which is why
-                // the makeHttpRequest(URL url) method signature specifies than an IOException
-                // could be thrown.
-                inputStream.close();
-            }
-        }
-        return jsonResponse;
-    }
-
-    /**
-     * Convert the {@link InputStream} into a String which contains the
-     * whole JSON response from the server.
-     */
-    private static String readFromStream(InputStream inputStream) throws IOException {
-        StringBuilder output = new StringBuilder();
-        if (inputStream != null) {
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-            BufferedReader reader = new BufferedReader(inputStreamReader);
-            String line = reader.readLine();
-            while (line != null) {
-                output.append(line);
-                line = reader.readLine();
-            }
-        }
-        return output.toString();
-    }
 
     /**
      * Return a list of {@link Collection} objects that has been built up from
@@ -147,7 +71,10 @@ public final class QueryUtils {
         try {
 
             // Create a JSONObject from the JSON response string
-            JSONArray collectionArray = new JSONArray(collectionJSON);
+            JSONObject collectionJSONObject = new JSONObject(collectionJSON);
+
+            // Create JSONArray from the JSON Object
+            JSONArray collectionArray = new JSONArray(collectionJSONObject);
 
             // For each collection in the collectionArray, create an {@link Collection} object
             for (int i = 0; i < collectionArray.length(); i++) {
@@ -189,10 +116,10 @@ public final class QueryUtils {
             // If an error is thrown when executing any of the above statements in the "try" block,
             // catch the exception here, so the app doesn't crash. Print a log message
             // with the message from the exception.
-            Log.e("QueryUtils", "Problem parsing the collection JSON results", e);
+            Log.e("CollectionUtils", "Problem parsing the collection JSON results", e);
         }
 
-        // Return the list of earthquakes
+        // Return the list of collection
         return collections;
     }
 
